9949317c6c4ada663de43318054824b97671a43f
David <davidagamez04@gmail.com>
2025-03-28 21:23:37 -0500

    feat: Create Algorithms component

---
+++ src/algorithms.ts
@@ -0,0 +1,18 @@
+export const algorithms = [
+  {
+    href: '/fundamentals',
+    name: 'Fundamentals',
+    description:
+      'Learn the basics have to know to start your journey in algorithms.',
+  },
+  {
+    href: '/search',
+    name: 'Search',
+    description: 'Learn how to search for an element in a collection of data.',
+  },
+  {
+    href: '/problems',
+    name: 'Problems',
+    description: 'Interesing problems with algorithms.',
+  },
+]
