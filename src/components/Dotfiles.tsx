7b7e2768f395c1ece2f1f6d4ac82ad8530c69a0d
David <davidagamez04@gmail.com>
2025-03-29 11:31:12 -0500

    refactor Update __curl description and refactor footer

--- src/components/Dotfiles.tsx
+++ src/components/Dotfiles.tsx
@@ -1,7 +1,8 @@
 import { Button } from '@/components/Button'
 import { Heading } from '@/components/Heading'
-
+import { GitHubIcon } from './icons/GithubIcon'
 import { dotfiles } from '@/dotfiles'
+import { SocialLink } from './SocialLink'
 
 export function Dotfiles() {
   return (
@@ -9,6 +10,19 @@ export function Dotfiles() {
       <Heading level={2} id="dotfiles">
         Dotfiles
       </Heading>
+
+      <p className="mt-4 flex items-center gap-1 text-zinc-600 dark:text-zinc-400">
+        <SocialLink
+          href="https://github.com/entr0phy4/.dotfiles"
+          icon={GitHubIcon}
+        >
+          Follow us on GitHub
+        </SocialLink>
+        Dotfiles are configuration files that are used to customize and
+        personalize your system. Here are some of the dotfiles that I use on a
+        daily basis.
+      </p>
+
       <div className="not-prose mt-4 grid grid-cols-1 gap-8 border-t border-zinc-900/5 pt-10 sm:grid-cols-2 xl:grid-cols-4 dark:border-white/5">
         {dotfiles.map((dotfile) => (
           <div key={dotfile.href}>
